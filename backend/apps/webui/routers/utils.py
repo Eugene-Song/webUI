5b6ae0bd0cded124ca2becbbf8487220e374fe6c
Yuxin Song <ys385@duke.edu>
2024-07-05 00:36:30 -0700

    not test backend api for downloading members

--- backend/apps/webui/routers/utils.py
+++ backend/apps/webui/routers/utils.py
@@ -4,11 +4,14 @@ from peewee import SqliteDatabase
 from starlette.responses import StreamingResponse, FileResponse
 from pydantic import BaseModel
 
+from io import StringIO
+import csv
 
 from fpdf import FPDF
 import markdown
 import black
 
+from apps.webui.models.users import Users
 
 from apps.webui.internal.db import DB
 from utils.utils import get_admin_user
@@ -124,6 +127,40 @@ async def download_db(user=Depends(get_admin_user)):
         media_type="application/octet-stream",
         filename="webui.db",
     )
+
+
+@router.get("/db/downloadMembers")
+async def download_members(user=Depends(get_admin_user)):
+    if not ENABLE_ADMIN_EXPORT:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=ERROR_MESSAGES.ACCESS_PROHIBITED,
+        )
+    if not isinstance(DB, SqliteDatabase):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=ERROR_MESSAGES.DB_NOT_SQLITE,
+        )
+
+    def iter_csv():
+        output = StringIO()
+        writer = csv.writer(output)
+
+        # Get the field names from the User model
+        field_names = [field.name for field in Users._meta.sorted_fields]
+        writer.writerow(field_names)
+
+        # set default parameters for the get_users method
+        users = Users.get_users()
+        for user in users:
+            writer.writerow([getattr(user, field) for field in field_names])
+            yield output.getvalue()
+            output.seek(0)
+            output.truncate(0)
+
+    response = StreamingResponse(iter_csv(), media_type="text/csv")
+    response.headers["Content-Disposition"] = "attachment; filename=users.csv"
+    return response
 
 
 @router.get("/litellm/config")
